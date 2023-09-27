eaeb6326311795d44c8bb064784b1262f044b8c5
jiaYun516 <05162006keke@gmail.com>
2023-09-27 15:47:03 +0800

    123

--- djangoMblog/settings.py
+++ djangoMblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uj^-p(d6hy+d2^_0*2vo84q0bib^5$s9+8fz5-%=@4_m*xdsm+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog1'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 

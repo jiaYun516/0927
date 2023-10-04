e7dd827c6d641f7789f67bca059d561a23378804
jiaYun516 <05162006keke@gmail.com>
2023-10-04 16:30:34 +0800

    1004-1

--- blog1/admin.py
+++ blog1/admin.py
@@ -6,6 +6,7 @@ from blog1.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "pub_date")
+    #當你訪問管理站點並查看 Post 模型時，將會按照 list_display 屬性指定的順序和字段顯示數據
 
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post, PostAdmin) 
